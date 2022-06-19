7f861e0f2fced069b15be34bf11dfa7aa4bce79d
shakuurally <abdishakuuralimohamed@gmail.com>
2022-06-19 17:32:56 +0300

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="w-screen flex flex-col items-center justify-center text-center space-y-5 p-14 text-gray-200 bg-blue-500">
-      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit ametd0 </p>
       <h2 className="text-2xl ">stay up-todate with what we are doign</h2>
       <div className="space-x-5 space-y-6">
         <input
